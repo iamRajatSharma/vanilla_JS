226869102ad00692b6766d1c65b746bc77689748
= <=>
2024-01-20 23:03:57 +0530

    first commit

---
+++ dom/script.js
@@ -0,0 +1,8 @@
+(function () {
+    let btnPress = document.getElementById("btn");
+    btnPress.addEventListener("click", function(){
+        var storeInput = document.getElementById("name").value;
+        alert(storeInput)
+    })
+
+})();
